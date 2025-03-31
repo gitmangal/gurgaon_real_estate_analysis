f24ac7df97515693ada4b963d54b17e03268d71f
Mangla gupta <135738278+gitmangal@users.noreply.github.com>
2025-03-31 12:53:01 +0530

    Update Price Predictor.py

--- streamlit_app/pages/Price Predictor.py
+++ streamlit_app/pages/Price Predictor.py
@@ -6,35 +6,35 @@ import pandas as pd
 
 st.title("hello this is Gurgaon Property Website")
 
-with open("C:/Users/mangal/Downloads/df.pkl","rb") as file:
-    data = pickle.load(file)
+# with open("C:/Users/mangal/Downloads/df.pkl","rb") as file:
+#     data = pickle.load(file)
 
-with open("C:/Users/mangal/Downloads/pipeline.pkl","rb") as ml_modl:
-    pipline = pickle.load(ml_modl)
+# with open("C:/Users/mangal/Downloads/pipeline.pkl","rb") as ml_modl:
+#     pipline = pickle.load(ml_modl)
 
-df = pd.DataFrame(data)
-#st.dataframe(df)
+#df = pd.DataFrame(data)
+# #st.dataframe(df)
 
-st.header("Select your Input")
+# st.header("Select your Input")
 
-property_type = st.selectbox("Property type",["house","flate"])
-sector = st.selectbox("Select sector",df['sector'].unique().tolist())
-bedroom = st.selectbox("Number of Bedrooms",sorted(df['bedRoom'].unique().tolist()))
-bathroom = st.selectbox("Number of Bathrooms",sorted(df['bathroom'].unique().tolist()))
-balcony = st.selectbox("Number of Balcony",sorted(df['balcony'].unique().tolist()))
-Property_age = st.selectbox("Property Age",sorted(df['agePossession'].unique().tolist()))
-builtup_area =  float(st.number_input('Built up area'))
-servant_room = float(st.selectbox("Servant Room", [0,1]))
-store_room = float(st.selectbox("Servant Room", [0,1], key="servant_room_key"))
-furnishing_type = st.selectbox("Furnishing Type", sorted(df['furnishing_type'].unique().tolist()))
-luxury_category = st.selectbox("Luxury Category", sorted(df['luxury_category'].unique().tolist()))
-floor_category = st.selectbox("Floor Category", sorted(df['floor_category'].unique().tolist()))
+# property_type = st.selectbox("Property type",["house","flate"])
+# sector = st.selectbox("Select sector",df['sector'].unique().tolist())
+# bedroom = st.selectbox("Number of Bedrooms",sorted(df['bedRoom'].unique().tolist()))
+# bathroom = st.selectbox("Number of Bathrooms",sorted(df['bathroom'].unique().tolist()))
+# balcony = st.selectbox("Number of Balcony",sorted(df['balcony'].unique().tolist()))
+# Property_age = st.selectbox("Property Age",sorted(df['agePossession'].unique().tolist()))
+# builtup_area =  float(st.number_input('Built up area'))
+# servant_room = float(st.selectbox("Servant Room", [0,1]))
+# store_room = float(st.selectbox("Servant Room", [0,1], key="servant_room_key"))
+# furnishing_type = st.selectbox("Furnishing Type", sorted(df['furnishing_type'].unique().tolist()))
+# luxury_category = st.selectbox("Luxury Category", sorted(df['luxury_category'].unique().tolist()))
+# floor_category = st.selectbox("Floor Category", sorted(df['floor_category'].unique().tolist()))
 
-if st.button("Predict"):
-    data = [[property_type,sector,bedroom,bathroom,balcony ,Property_age,builtup_area,servant_room,store_room,furnishing_type,luxury_category,floor_category]]
-    columns = ['property_type', 'sector', 'bedRoom', 'bathroom', 'balcony',
-       'agePossession', 'built_up_area', 'servant room', 'store room',
-       'furnishing_type', 'luxury_category', 'floor_category']
-    one_df = pd.DataFrame(data,columns=columns)
-    st.dataframe(one_df)
-    price = np.expm1(pipline.predict(one_df))
+# if st.button("Predict"):
+#     data = [[property_type,sector,bedroom,bathroom,balcony ,Property_age,builtup_area,servant_room,store_room,furnishing_type,luxury_category,floor_category]]
+#     columns = ['property_type', 'sector', 'bedRoom', 'bathroom', 'balcony',
+#        'agePossession', 'built_up_area', 'servant room', 'store room',
+#        'furnishing_type', 'luxury_category', 'floor_category']
+#     one_df = pd.DataFrame(data,columns=columns)
+#     st.dataframe(one_df)
+#     price = np.expm1(pipline.predict(one_df))
